4a839029e2caf030bc7e16e1e49d786a7a60ad8e
arseniy <ar.prorokov@gmail.com>
2022-10-06 22:57:29 +0300

    fixed sussy animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,36 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    private Rigidbody2D rb;
+    private float HorizontalSpeed = 0;
+    private float VerticalSpeed = 0;
     [SerializeField] private float speed = 5;
+
+
+    [Header("Animation settings")]
+    public Animator animator;
     
     private bool facingRight = true;
     
     // Start is called before the first frame update
     private void Awake()
     {
+        rb = GetComponent<Rigidbody2D>();
         SetZeroPos();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
-            Run();
+
+
+
+    }
+    private void FixedUpdate() {
+        Run();
+
+        animator.SetFloat("IsMoving", Math.Abs(HorizontalSpeed) + Math.Abs(VerticalSpeed));
     }
 
     private void Run()
     {
-        Vector3 dir  = transform.right * Input.GetAxis("Horizontal") + transform.up * Input.GetAxis("Vertical");
+        HorizontalSpeed = Input.GetAxisRaw("Horizontal") * speed;
+        VerticalSpeed = Input.GetAxisRaw("Vertical") * speed;
+        Vector2 TargetVelocity = new Vector2(HorizontalSpeed, VerticalSpeed);
+        rb.velocity = TargetVelocity.normalized * speed;
 
-        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed*Time.deltaTime);
-
-        if (dir.x < 0 && facingRight)
+        if (rb.velocity.x < 0 && facingRight)
             Flip();
-        if (dir.x > 0 && !facingRight)
+        if (rb.velocity.x > 0 && !facingRight)
             Flip();
 
     }
